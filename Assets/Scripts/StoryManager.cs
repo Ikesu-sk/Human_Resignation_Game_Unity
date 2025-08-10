d3f37e50776d5fab133d66c8cf69ce2bb9452ba7
Ikesu-sk <skano1250@gmail.com>
2025-08-10 23:02:59 +0900

    2025-08-10

--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 using System.Collections;
 using Unity.VisualScripting;
 using System;
+using UnityEditor.U2D.Animation;
 
 public class StoryManager : MonoBehaviour
 {
@@ -20,7 +21,9 @@ public class StoryManager : MonoBehaviour
     public int storyIndex { get; private set; } //get; private set;：外から参照できるけど値は入れられないよ　
     public int textIndex { get; private set; }
 
-    private bool finishText = false;
+    // 一回目の文章時のみ、Enterキーを一回押せばfulltextが表示されるようにする。
+    private int enterPressCount = 1;
+    private bool typingtext = false;
 
     // SoundManagerクラスのscriptを受け付けるためのコード
     [SerializeField] private SoundManager soundManager;
@@ -35,7 +38,7 @@ public class StoryManager : MonoBehaviour
 
     private void Update()
     {
-        if (Keyboard.current.enterKey.wasPressedThisFrame && finishText)
+        if (Keyboard.current.enterKey.wasPressedThisFrame && !typingtext)
         {
             textIndex++;
             storyText.text = "";
@@ -48,10 +51,22 @@ public class StoryManager : MonoBehaviour
         // 自作関数(soundmanager.cs)を実行するコード
         soundManager.PlayBGM(storyDatas[_storyIndex].bgm);
 
+        // storyDatasのx番目のstoryIndexの、Y番目のstoriesの中身を取り出す
         var storyElement = storyDatas[_storyIndex].stories[_textIndex];
 
         // 画面上にある各要素にstoryDatsから取得したデータを格納
         background.sprite = storyElement.Background;
+
+        // characteerimageの中身が存在した場合
+        if (storyElement.CharacterImage != null)
+        {
+            characterImage.gameObject.SetActive(true);
+            characterImage.sprite = storyElement.CharacterImage;
+        }
+        else
+        {
+            characterImage.gameObject.SetActive(false);
+        }
         characterImage.sprite = storyElement.CharacterImage;
         characterName.text = storyElement.CharacterName;
         // storyText.text = storyElement.StoryText;
@@ -81,16 +96,34 @@ public class StoryManager : MonoBehaviour
 
     private IEnumerator TypeSentence(int _storyIndex, int _textIndex)
     {
-
-        finishText = false; // ← ここで一度リセット！
+        
+        // 全文を取得しておく
+        string fullSentence = storyDatas[_storyIndex].stories[_textIndex].StoryText;
 
         // 一文字ずつ分割したものをletterに入れている
-        foreach (var letter in storyDatas[_storyIndex].stories[_textIndex].StoryText.ToCharArray())
+        foreach (var letter in fullSentence.ToCharArray())
         {
+            typingtext = true;
+            // もし、このフレーム中にEnterキーが押されたら
+            if (Keyboard.current.enterKey.wasPressedThisFrame)
+            {
+                enterPressCount++;
+
+                if (enterPressCount >= 2)
+                {
+                    // 全文を即座に表示
+                    storyText.text = fullSentence;
+                    Debug.Log("Enterキーが２回押されました");
+                    break;
+                }
+            }
             storyText.text += letter;
             yield return new WaitForSeconds(0.1f);
         }
 
-        finishText = true;
+        storyText.text = fullSentence;
+        Debug.Log("全表示storyText");
+        enterPressCount = 0;
+        typingtext = false;
     }    
 }
